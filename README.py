68f20943609ce4a0903e37a2d2b6e70fbeacfc41
Mohammed Olaiya <mohammedolaiya@gmail.com>
2021-05-04 21:33:46 -0400

    Update README.py

--- README.py
+++ README.py
@@ -5,3 +5,7 @@ import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
+print(Fore.ORANGE+Back.BLACK+"Welcome to this new app. " + Fore.GREEN+Back.White + "You can now set your settings.")
+print(Fore.RED+"You can now customize your app any how you want too.")
+print(Back.GREEN + Back.WHITE"Your username can be whatever you want it to be.")
+print(Fore.BROWN + "How many licks does it take to get to the center of a tootsie pop?")
